219c59f3ee54536e09b8e7eb8e8d67364e136725
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-12-29 16:52:10 +0530

    Enjoy

---
+++ christmastree.py
@@ -0,0 +1,63 @@
+import threading
+import os
+import random
+import time
+
+tree = list(open('tree.txt').read().rstrip())
+
+mutex = threading.Lock()
+
+
+def colored_dot(color):
+	if color == 'red':
+		return f'\033[91m⏺\033[0m'
+	if color == 'green':
+		return f'\033[92m⏺\033[0m'
+	if color == 'yellow':
+		return f'\033[93m⏺\033[0m'
+	if color == 'blue':
+		return f'\033[94m⏺\033[0m'
+
+def lights(color, indexes):
+	off = True
+	while True:
+		for idx in indexes:
+			tree[idx] = colored_dot(color) if off else '⏺'
+
+		mutex.acquire()
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("".join(tree))
+		mutex.release()
+	
+		off = not off
+
+		time.sleep(random.uniform(.5, 1.5))		
+
+yellow = []
+red = []
+blue = []
+green = []
+
+for i,c in enumerate(tree):
+	if tree[i] == 'Y':
+		yellow.append(i)
+		tree[i] = '⏺'
+	if tree[i] == 'R':
+		tree[i] = '⏺'
+		red.append(i)
+	if tree[i] == 'G':
+		green.append(i)
+		tree[i] = '⏺'
+	if tree[i] == 'B':
+		blue.append(i)
+		tree[i] = '⏺'
+
+ty = threading.Thread(target=lights, args=('yellow', yellow), daemon=True)
+tr = threading.Thread(target=lights, args=('red', red), daemon=True)
+tg = threading.Thread(target=lights, args=('green', green), daemon=True)
+tb = threading.Thread(target=lights, args=('blue', blue), daemon=True)
+
+for t in [ty, tr, tg, tb]:
+	t.start()
+for t in [ty, tr, tg, tb]:
+	t.join()
